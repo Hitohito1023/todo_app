73850a4b1187bd491bc98b313efa6cf2f2e6037b
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-02-05 18:32:43 +0900

    goalコントローラー記載

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,18 +1,17 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_goal, only: %i[ show edit update destroy ]
 
   # GET /goals or /goals.json
   def index
-    @goals = Goal.all
+    @goals = current_user.goals.all
   end
 
-  # GET /goals/1 or /goals/1.json
-  def show
-  end
+ 
 
   # GET /goals/new
   def new
-    @goal = Goal.new
+    @goal = current_user.goals.new
   end
 
   # GET /goals/1/edit
@@ -21,45 +20,33 @@ class GoalsController < ApplicationController
 
   # POST /goals or /goals.json
   def create
-    @goal = Goal.new(goal_params)
+    @goal = current_usesr.goals.new(goal_params)
 
-    respond_to do |format|
-      if @goal.save
-        format.html { redirect_to @goal, notice: "Goal was successfully created." }
-        format.json { render :show, status: :created, location: @goal }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @goal.errors, status: :unprocessable_entity }
-      end
+    if @goal.save
+      @status = true
+    else
+      @status = false
     end
   end
 
-  # PATCH/PUT /goals/1 or /goals/1.json
-  def update
-    respond_to do |format|
-      if @goal.update(goal_params)
-        format.html { redirect_to @goal, notice: "Goal was successfully updated." }
-        format.json { render :show, status: :ok, location: @goal }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @goal.errors, status: :unprocessable_entity }
-      end
-    end
+ # PATCH/PUT /goals/1
+ def update
+  if @goal.update(goal_params)
+     @status = true
+  else
+     @status = false
   end
-
+end
   # DELETE /goals/1 or /goals/1.json
   def destroy
     @goal.destroy
-    respond_to do |format|
-      format.html { redirect_to goals_url, notice: "Goal was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_goal
-      @goal = Goal.find(params[:id])
+      @goal = current_user.goals.find_by(id: params[:id])
+      redirect_to(goals_url, alert: "ERROR!!") if @goal.blank?
     end
 
     # Only allow a list of trusted parameters through.
